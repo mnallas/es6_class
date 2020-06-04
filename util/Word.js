e2aa4977c6a0fdbe56d33fce2f9e7ee34a675f58
myhkas <mmyhkas@gmail.com>
2020-06-03 20:02:54 -0700

    added reverse constructor with test

--- util/Word.js
+++ util/Word.js
@@ -2,6 +2,11 @@ class Word {
   constructor(wrd) {
     this.word = wrd;
   }
+
+  get rev() {
+    return new Word(this.word.split("").reverse().join(""));
+  }
+
   get getWord() {
     return this.word;
   }
